2ae5c8adabb9547855e12bf075298ef68d5c4b5b
vjacheslavkovalenko <64063853+vjacheslavkovalenko@users.noreply.github.com>
2024-12-03 00:14:34 +0300

    Delete app/src/main/java/by/vjacheslavkovalenko/asteroidstms/network directory

--- app/src/main/java/by/vjacheslavkovalenko/asteroidstms/network/entity/AsteroidDate.kt
+++
@@ -1,7 +0,0 @@
-package by.vjacheslavkovalenko.asteroidstms.network.entity
-
-data class AsteroidDate(
-
-    val formAsteroidResponse: List<AsteroidResponse>
-
-)
--- app/src/main/java/by/vjacheslavkovalenko/asteroidstms/network/entity/EstimatedDiameter.kt
+++
@@ -1,9 +0,0 @@
-package by.vjacheslavkovalenko.asteroidstms.network.entity
-
-import com.google.gson.annotations.SerializedName
-
-data class EstimatedDiameter(
-    @SerializedName("kilometers")
-    val kilometers: KilometersDiameter
-//    val miles: Miles
-)
